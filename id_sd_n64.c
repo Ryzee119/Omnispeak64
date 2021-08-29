9c93b84816463cb508bdff661d942f868b0654d1
Ryzee119 <wendland@live.com.au>
2021-08-30 06:46:15 +0930

    Audio: PC Speaker driver now works

--- id_sd_n64.c
+++ id_sd_n64.c
@@ -30,105 +30,112 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "id_sd.h"
 #include "ck_cross.h"
 
-#define SND_USE_LOOKUP
-#define PC_PIT_RATE 1193182
-#define SD_SFX_PART_RATE 140
-#define SD_SOUND_PART_RATE_BASE 1192030
+
+static const int PC_PIT_RATE = 1193182;
+static const int SD_SFX_PART_RATE = 140;
+static const int SD_SOUND_PART_RATE_BASE = 1192030;
+static const int BITRATE = 9600;
+uint16_t ints_per_sec;
 
 static int16_t *stream = NULL;
-static const int BITRATE = 9600;
 static bool SD_N64_IsLocked = false;
 
 //Timing backend for the gamelogic which uses the sound system
 static timer_link_t *t0_timer;
 void SDL_t0Service(void);
 
+enum backend
+{
+    NONE,
+    ADLIB,
+    SPEAKER
+};
+enum backend backend_t = NONE;
+
 //OPL (for Adlib backend)
 static opl3_chip nuked_oplChip;
 static bool SD_N64_AudioSubsystem_Up = false;
 static bool generate_stream = false;
 
+//Beep Driver (For PC Speaker backend)
+static bool beep_on;
+static int16_t beep_current_sample;
+static uint32_t beep_half_cycle_cnt, beep_half_cycle_cnt_max;
+static uint32_t beep_samples_per_service;
+static int beep_cursor = 0;
+
 //Audio interrupts
 static void _t0service(int ovfl)
 {
     SDL_t0Service();
+
+    if (backend_t == ADLIB)
+    {
+        //int chunk = audio_get_buffer_length() * ints_per_sec / BITRATE;
+        //OPL3_GenerateStream(&nuked_oplChip, &stream[beep_cursor], chunk);
+        //beep_cursor = (beep_cursor + (chunk * 2)) % (audio_get_buffer_length() * 2);
+        return;
+    }
+    //Each service a number of audio samples is generated (beep_samples_per_service)
+    //Put this in the audio stream
+    for (int i = 0; i < (beep_samples_per_service * 2); i += 2)
+    {
+        if (backend_t != SPEAKER)
+        {
+            break;
+        }
+        if (!beep_on)
+        {
+            stream[beep_cursor + 0] = 0;
+            stream[beep_cursor + 1] = 0;
+        }
+        else
+        {
+            stream[beep_cursor + 0] = beep_current_sample;
+            stream[beep_cursor + 1] = beep_current_sample;
+            beep_half_cycle_cnt += 2 * PC_PIT_RATE;
+            if (beep_half_cycle_cnt >= beep_half_cycle_cnt_max)
+            {
+                beep_half_cycle_cnt %= beep_half_cycle_cnt_max;
+                beep_current_sample = 0x5FFF - beep_current_sample;
+            }
+        }
+        beep_cursor = (beep_cursor + 2) % (audio_get_buffer_length() * 2);
+    }
 }
 
 static void _audio_callback(short *buffer, size_t numsamples)
 {
-    memcpy(buffer, stream, numsamples * 2);
+    for (int i = 0; i < (numsamples * 2); i += 2)
+    {
+        int j = (beep_cursor + i) % (numsamples * 2);
+        buffer[i] = stream[j];
+        buffer[i + 1] = stream[j + 1];
+    }
 }
 
 void SD_N64_SetTimer0(int16_t int_8_divisor)
 {
     //Create an interrupt that occurs at a certain frequency.
-    uint16_t ints_per_sec = SD_SOUND_PART_RATE_BASE / int_8_divisor;
+    ints_per_sec = SD_SOUND_PART_RATE_BASE / int_8_divisor;
     delete_timer(t0_timer);
     t0_timer = new_timer(TIMER_TICKS(1000000 / ints_per_sec), TF_CONTINUOUS, _t0service);
+    beep_samples_per_service = int_8_divisor * BITRATE / PC_PIT_RATE;
 }
 
 void SD_N64_alOut(uint8_t reg, uint8_t val)
 {
+    backend_t = ADLIB;
     OPL3_WriteReg(&nuked_oplChip, reg, val);
-    if (audio_can_write() && generate_stream)
-    {
-        OPL3_GenerateStream(&nuked_oplChip, stream, audio_get_buffer_length());
-        generate_stream = false;
-    }
 }
-
-//Create a sine lookup table to speed up the sine generation. 200 positions should be reasonably accurate
-//for what we're doing here. Capped at 16383 which is what upstream does.
-static const int16_t sin_lookup[] = {
-0,515,515,1029,1542,2053,2563,3070,3574,4074,4571,5063,5550,6031,6506,
-6976,7438,7438,7893,8340,8778,9209,9630,10041,10443,10834,11215,11585,
-11943,12289,12289,12623,12945,13254,13550,13833,14102,14357,14597,14824,
-15036,15233,15233,15414,15581,15732,15868,15988,16093,16181,16254,16310,
-16351,16375,16375,16383,16375,16351,16310,16254,16181,16093,15988,15868,
-15732,15581,15581,15414,15233,15036,14824,14597,14357,14102,13833,13550,
-13254,12945,12945,12623,12289,11943,11585,11215,10834,10443,10041,9630,
-9209,8778,8778,8340,7893,7438,6976,6506,6031,5550,5063,4571,4074,3574,
-3070,2563,2563,2053,1542,1029,515,0,-515,-1029,-1542,-2053,-2563,-3070,
--3574,-4074,-4074,-4571,-5063,-5550,-6031,-6506,-6976,-7438,-7893,-8340,
--8778,-9209,-9209,-9630,-10041,-10443,-10834,-11215,-11585,-11943,-12289,
--12623,-12945,-12945,-13254,-13550,-13833,-14102,-14357,-14597,-14824,-15036,
--15233,-15414,-15414,-15581,-15732,-15868,-15988,-16093,-16181,-16254,-16310,
--16351,-16375,-16375,-16383,-16375,-16351,-16310,-16254,-16181,-16093,-15988,
--15868,-15732,-15732,-15581,-15414,-15233,-15036,-14824,-14597,-14357,-14102,
--13833,-13550,-13550,-13254,-12945,-12623,-12289,-11943,-11585,-11215,-10834,
--10443,-10041,-10041,-9630,-9209,-8778,-8340,-7893,-7438,-6976,-6506,
--6031,-5550,-5550,-5063,-4571,-4074,-3574,-3070,-2563,-2053,-1542,-1029,-515,
-};
 
 void SD_N64_PCSpkOn(bool on, int freq)
 {
-    if (!on || freq == 0)
-    {
-        memset(stream, 0x00, 2 * sizeof(short) * audio_get_buffer_length());
-        return;
-    }
-
-    //Keep track of the current index position from previous call for a smoother sine.
-    static int running_val = 0;
-
-    //Fill the stream with a 16 bit interlaced stereo PCM waveform at the required frequency
-    //audio_get_buffer_length() returns the number of stereo samples. I mix it onto both channels.
-    int hz = SD_SOUND_PART_RATE_BASE / freq;
-    for (int i = 0; i < (audio_get_buffer_length() * 2); i += 2)
-    {
-#ifdef SND_USE_LOOKUP
-        const int lookup_len = sizeof(sin_lookup) / sizeof(sin_lookup[0]);
-        int index = ((hz * lookup_len / BITRATE * i / 2) + running_val) % lookup_len;
-        stream[i + 0] = sin_lookup[index];
-        stream[i + 1] = sin_lookup[index];
-        running_val = (running_val + 1) % lookup_len;
-#else
-        int sinus = 0x3FFF * sin((2 * M_PI * hz) * (i + running_val) / BITRATE / 2);
-        stream[i + 0] = sinus & 0xFFFF;
-        stream[i + 1] = sinus & 0xFFFF;
-        running_val = (running_val + 1) % audio_get_buffer_length();
-#endif
-    }
+    backend_t = SPEAKER;
+    beep_on = on;
+	beep_current_sample = 0;
+	beep_half_cycle_cnt = 0;
+	beep_half_cycle_cnt_max = BITRATE * freq;
 }
 
 void SD_N64_Startup(void)
