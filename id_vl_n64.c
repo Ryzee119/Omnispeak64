f314a40b36919e19a087260b37a89291a1d251eb
Ryzee119 <wendland@live.com.au>
2021-08-30 06:41:22 +0930

    Video: clean up and optimise

--- id_vl_n64.c
+++ id_vl_n64.c
@@ -11,7 +11,7 @@
 #include <string.h>
 #include <libdragon.h>
 
-#ifdef SW_RENDERER
+#ifndef USE_SW_RENDERER
 #define USE_HW_RENDERER
 #endif
 
@@ -23,11 +23,12 @@ typedef struct VL_N64_Surface
 } VL_N64_Surface;
 
 static display_context_t disp = 0;
+uint32_t border_colour = 0xFFFFFFFF;
 
 #ifdef USE_HW_RENDERER
 #include "n64_rdp/rdl.h"
 #include "n64_rdp/rdp_commands.h"
-#define NUM_DISPLAY_LISTS 3
+#define NUM_DISPLAY_LISTS 2
 static RdpDisplayList *dls[NUM_DISPLAY_LISTS] = {NULL};
 static RdpDisplayList *dl;
 uint16_t *palette;
@@ -43,14 +44,16 @@ static void VL_N64_SetVideoMode(int mode)
     {
         init_interrupts();
         display_init(RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, ANTIALIAS_RESAMPLE);
+
+        //Scale to EVGA 320x200 to 320x240. Original DOS version is 4:3
+        uint32_t *vi_base = (uint32_t *)0xA4400000;
+        vi_base[13] = 0x01000000 | (1024 * VL_EGAVGA_GFX_HEIGHT / 240);
+
 #ifdef USE_HW_RENDERER
         rdp_init();
         for (int i = 0; i < NUM_DISPLAY_LISTS; i++)
         {
             dls[i] = rdl_heap_alloc(2048);
-        }
-        for (int i = 0; i < NUM_DISPLAY_LISTS; i++)
-        {
             rdl_reset(dls[i]);
         }
         dl = dls[0];
@@ -63,7 +66,6 @@ static void VL_N64_SetVideoMode(int mode)
         staging_sprite->hslices = 1;
         staging_sprite->vslices = 1;
 #endif
-        
     }
     else
     {
@@ -94,7 +96,7 @@ static void VL_N64_DestroySurface(void *surface)
 {
     VL_N64_Surface *surf = (VL_N64_Surface *)surface;
     if (surf->pixels)
-        free(surf->pixels); //FIXME free aligned memory?
+        free(surf->pixels);
     free(surf);
 }
 
@@ -115,23 +117,27 @@ static void VL_N64_GetSurfaceDimensions(void *surface, int *w, int *h)
 
 static void VL_N64_RefreshPaletteAndBorderColor(void *screen)
 {
+    uint8_t r, g, b;
+    r = VL_EGARGBColorTable[vl_emuegavgaadapter.bordercolor][0];
+    g = VL_EGARGBColorTable[vl_emuegavgaadapter.bordercolor][1];
+    b = VL_EGARGBColorTable[vl_emuegavgaadapter.bordercolor][2];
+    border_colour = ((r >> 3) << 11) | ((g >> 3) << 6) | ((b >> 3) << 1) | 0x01; //rgba 5551
 #ifdef USE_HW_RENDERER
     for (int i = 0; i < 16; i++)
     {
-        uint8_t r = VL_EGARGBColorTable[vl_emuegavgaadapter.palette[i]][0];
-        uint8_t g = VL_EGARGBColorTable[vl_emuegavgaadapter.palette[i]][1];
-        uint8_t b = VL_EGARGBColorTable[vl_emuegavgaadapter.palette[i]][2];
+        r = VL_EGARGBColorTable[vl_emuegavgaadapter.palette[i]][0];
+        g = VL_EGARGBColorTable[vl_emuegavgaadapter.palette[i]][1];
+        b = VL_EGARGBColorTable[vl_emuegavgaadapter.palette[i]][2];
         uint16_t c = ((r >> 3) << 11) | ((g >> 3) << 6) | ((b >> 3) << 1) | 0x01; //rgba 5551
         palette[i] = c;
     }
-
+    data_cache_hit_writeback_invalidate(palette, 16 * 2);
+    //Load the palette into TMEM
     rdl_push(dl,
              RdpSyncTile(),
              MRdpLoadPalette16(2, (uint32_t)palette, RDP_AUTO_TMEM_SLOT(pal_slot)),
              RdpSyncTile()
-        );
-
-    data_cache_hit_writeback_invalidate(palette, 16 * 2);
+    );
 #endif
 }
 
@@ -148,6 +154,19 @@ static void VL_N64_SurfaceRect(void *dst_surface, int x, int y, int w, int h, in
     {
         memset(((uint8_t *)surf->pixels) + _y * surf->width + x, colour, w);
     }
+    /*
+    uint32_t _colour = (colour & 0xFF) << 24 | (colour & 0xFF) << 16 | (colour & 0xFF) << 8 | (colour & 0xFF);
+    rdl_push(dl,
+             RdpSetColorImage(RDP_TILE_FORMAT_INDEX, RDP_TILE_SIZE_8BIT, surf->width, (uint32_t)surf->pixels),
+             RdpSetClippingI(0, 0, surf->width, surf->height),
+             RdpSyncPipe(),
+             RdpSyncTile(),
+             RdpSetOtherModes(SOM_CYCLE_FILL),
+             RdpSetFillColor(_colour),
+             RdpFillRectangleI(x, y, 10, 10)
+    );
+    data_cache_hit_writeback_invalidate(surf->pixels,  surf->width * surf->height);
+    */
 }
 
 static void VL_N64_SurfaceRect_PM(void *dst_surface, int x, int y, int w, int h, int colour, int mapmask)
@@ -299,38 +318,55 @@ static void VL_N64_Present(void *surface, int scrlX, int scrlY, bool singleBuffe
     //debugf("VL_N64_Present w: %d h: %d scrlx: %d scrly: %d\n", src->width, src->height, scrlX, scrlY);
     while (!(disp = display_lock())) {}
 #ifdef USE_HW_RENDERER
-    extern void *__safe_buffer[];
-    rdp_attach_display(disp);
-    rdp_set_clipping(0, 0, 320, 220);
-    rdl_push(dl,
-             RdpSyncPipe(),
-             // By default, go into COPY mode which is used by most sprites
-             RdpSetOtherModes(SOM_CYCLE_COPY | SOM_ALPHA_COMPARE | SOM_ENABLE_TLUT_RGB16),
-             // Setup combiner for texture output without lighting. We need 1-cycle
-             // mode for sprites with scaling or flipping, so the combiner must be
-             // configured.
-             RdpSetCombine(Comb1_Rgb(ZERO, ZERO, ZERO, TEX0), Comb1_Alpha(ZERO, ZERO, ZERO, TEX0)),
-             RdpSetBlendColor(0x0001) // alpha threshold value = 1
-    );
+    static const float scale_x = 1;
+    static const float scale_y = 1.2;
 
-    int rdp_tex_slot = 0;
-    int current_y = 0;
-    int y_per_loop = 4;
+    //We draw the screen from top to bottom.
+    //We can only draw 2048bytes per loop and for simplicity we want it to be a multiple
+    //of the width. Start at 5 rows per loop.
+    //If the width * 5 is larger than 2048, try less rows until we're ok.
     int x_per_loop = src->width;
+    int y_per_loop = 5;
+    int current_y = 0;
     int chunk_size = x_per_loop * y_per_loop;
+    while (chunk_size > 2048 && y_per_loop)
+    {
+        y_per_loop--;
+        chunk_size -= x_per_loop;
+    }
+    assert(y_per_loop > 0);
     assert(chunk_size <= 2048);
+
+    rdp_attach_display(disp);
+    rdp_set_clipping(0, 0, 320, CK_Cross_min(200, src->height));
+
+    //Do we need to do this?
+    /*rdl_push(dl,
+             RdpSyncPipe(),
+             RdpSetOtherModes(SOM_CYCLE_FILL),
+             RdpSetFillColor16(border_colour),
+             RdpFillRectangleI(0, 0, 320, 240)
+    );*/
+
+    rdl_push(dl,
+                RdpSyncPipe(),
+                RdpSetOtherModes(SOM_CYCLE_COPY | SOM_ALPHA_COMPARE | SOM_ENABLE_TLUT_RGB16)
+    );
     data_cache_hit_writeback_invalidate(src->pixels,  src->width * src->height);
     for(int i = 0; i < ((src->width * src->height) / chunk_size); i++)
     {
+        //Load y_per_loop * x_per_loop pixels into TMEM, and apply palette from pal_slot
         rdl_push(dl,
-                MRdpLoadTex8bpp(0, (uint32_t)src->pixels + (chunk_size * i), x_per_loop, y_per_loop, x_per_loop, RDP_AUTO_TMEM_SLOT(rdp_tex_slot), RDP_AUTO_PITCH),
-                MRdpSetTile8bpp(1, RDP_AUTO_TMEM_SLOT(rdp_tex_slot), RDP_AUTO_PITCH, RDP_AUTO_TMEM_SLOT(pal_slot), x_per_loop, y_per_loop)
+                MRdpLoadTex8bpp(0, (uint32_t)src->pixels + (chunk_size * i), x_per_loop, y_per_loop, x_per_loop, RDP_AUTO_TMEM_SLOT(0), RDP_AUTO_PITCH),
+                MRdpSetTile8bpp(1, RDP_AUTO_TMEM_SLOT(0), RDP_AUTO_PITCH, RDP_AUTO_TMEM_SLOT(pal_slot), x_per_loop, y_per_loop)
         );
+
+        //Draw the texture to screen. The drawing is offset by scrlX and scrY
         int sw = x_per_loop, sh = y_per_loop;
-        int x0 = 0, y0 = current_y;//CK_Cross_max(0, current_y - scrlY);
+        int x0 = 0, y0 = CK_Cross_max(0, current_y - scrlY);
         rdl_push(dl,
-                RdpTextureRectangle1I(1, x0, y0, x0 + sw - 1, y0 + sh - 1),
-                RdpTextureRectangle2I(0, 0, 4, 1)
+                    RdpTextureRectangle1I(1, x0, y0, x0 + sw, y0 + y_per_loop),
+                    RdpTextureRectangle2I(scrlX, 0, 4, 1)
         );
         current_y += y_per_loop;
     }
@@ -341,14 +377,13 @@ static void VL_N64_Present(void *surface, int scrlX, int scrlY, bool singleBuffe
     {
         if (dl == dls[i])
         {
-            dl = dls[i + 1 == NUM_DISPLAY_LISTS ? 0 : i + 1];
+            dl = dls[(i + 1) % NUM_DISPLAY_LISTS];
             break;
         }
     }
     rdl_reset(dl);
-
     rdp_detach_display();
-#else
+#else //Software Renderer
     uint16_t *dest = (uint16_t *)staging_sprite->data;
     for (int _y = scrlY; _y < src->height; _y++)
     {
@@ -375,45 +410,44 @@ static void VL_N64_Present(void *surface, int scrlX, int scrlY, bool singleBuffe
     display_show(disp);
 }
 
-void VL_N64_FlushParams()
+static void VL_N64_FlushParams()
 {
 }
 
-void VL_N64_WaitVBLs(int vbls)
+static void VL_N64_WaitVBLs(int vbls)
 {
-    //graphics_draw_sprite already waits for vblank
-    //SD_SetTimeCount(SD_GetTimeCount() + CK_Cross_max(vbls,1));
+    //display_show already waits for vblank
 }
 
-// Unfortunately, we can't take advantage of designated initializers in C++.
 VL_Backend vl_n64_backend =
-    {
-        /*.setVideoMode =*/&VL_N64_SetVideoMode,
-        /*.createSurface =*/&VL_N64_CreateSurface,
-        /*.destroySurface =*/&VL_N64_DestroySurface,
-        /*.getSurfaceMemUse =*/&VL_N64_GetSurfaceMemUse,
-        /*.getSurfaceDimensions =*/&VL_N64_GetSurfaceDimensions,
-        /*.refreshPaletteAndBorderColor =*/&VL_N64_RefreshPaletteAndBorderColor,
-        /*.surfacePGet =*/&VL_N64_SurfacePGet,
-        /*.surfaceRect =*/&VL_N64_SurfaceRect,
-        /*.surfaceRect_PM =*/&VL_N64_SurfaceRect_PM,
-        /*.surfaceToSurface =*/&VL_N64_SurfaceToSurface,
-        /*.surfaceToSelf =*/&VL_N64_SurfaceToSelf,
-        /*.unmaskedToSurface =*/&VL_N64_UnmaskedToSurface,
-        /*.unmaskedToSurface_PM =*/&VL_N64_UnmaskedToSurface_PM,
-        /*.maskedToSurface =*/&VL_N64_MaskedToSurface,
-        /*.maskedBlitToSurface =*/&VL_N64_MaskedBlitToSurface,
-        /*.bitToSurface =*/&VL_N64_BitToSurface,
-        /*.bitToSurface_PM =*/&VL_N64_BitToSurface_PM,
-        /*.bitXorWithSurface =*/&VL_N64_BitXorWithSurface,
-        /*.bitBlitToSurface =*/&VL_N64_BitBlitToSurface,
-        /*.bitInvBlitToSurface =*/&VL_N64_BitInvBlitToSurface,
-        /*.scrollSurface =*/&VL_N64_ScrollSurface,
-        /*.present =*/&VL_N64_Present,
-        /*.getActiveBufferId =*/&VL_N64_GetActiveBufferId,
-        /*.getNumBuffers =*/&VL_N64_GetNumBuffers,
-        /*.flushParams =*/&VL_N64_FlushParams,
-        /*.waitVBLs =*/&VL_N64_WaitVBLs};
+{
+    .setVideoMode = &VL_N64_SetVideoMode,
+    .createSurface = &VL_N64_CreateSurface,
+    .destroySurface = &VL_N64_DestroySurface,
+    .getSurfaceMemUse = &VL_N64_GetSurfaceMemUse,
+    .getSurfaceDimensions = &VL_N64_GetSurfaceDimensions,
+    .refreshPaletteAndBorderColor = &VL_N64_RefreshPaletteAndBorderColor,
+    .surfacePGet = &VL_N64_SurfacePGet,
+    .surfaceRect = &VL_N64_SurfaceRect,
+    .surfaceRect_PM = &VL_N64_SurfaceRect_PM,
+    .surfaceToSurface = &VL_N64_SurfaceToSurface,
+    .surfaceToSelf = &VL_N64_SurfaceToSelf,
+    .unmaskedToSurface = &VL_N64_UnmaskedToSurface,
+    .unmaskedToSurface_PM = &VL_N64_UnmaskedToSurface_PM,
+    .maskedToSurface = &VL_N64_MaskedToSurface,
+    .maskedBlitToSurface = &VL_N64_MaskedBlitToSurface,
+    .bitToSurface = &VL_N64_BitToSurface,
+    .bitToSurface_PM = &VL_N64_BitToSurface_PM,
+    .bitXorWithSurface = &VL_N64_BitXorWithSurface,
+    .bitBlitToSurface = &VL_N64_BitBlitToSurface,
+    .bitInvBlitToSurface = &VL_N64_BitInvBlitToSurface,
+    .scrollSurface = &VL_N64_ScrollSurface,
+    .present = &VL_N64_Present,
+    .getActiveBufferId = &VL_N64_GetActiveBufferId,
+    .getNumBuffers = &VL_N64_GetNumBuffers,
+    .flushParams = &VL_N64_FlushParams,
+    .waitVBLs = &VL_N64_WaitVBLs
+};
 
 VL_Backend *VL_Impl_GetBackend()
 {
