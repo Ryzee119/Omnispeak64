4939b4641109e344e01a3460d7c4c1c5df722085
Ryzee119 <wendland@live.com.au>
2023-06-12 19:28:12 +0930

    Video: Update to latest rdpq

--- id_vl_n64.c
+++ id_vl_n64.c
@@ -10,16 +10,8 @@
 #include "id_vl_private.h"
 #include "ck_cross.h"
 
-typedef struct VL_N64_Surface
-{
-    VL_SurfaceUsage use;
-    int width, height;
-    uint8_t *pixels;
-} VL_N64_Surface;
+typedef surface_t VL_N64_Surface;
 
-static display_context_t disp = 0;
-static uint32_t display_width;
-static uint32_t display_height;
 static uint32_t border_colour = 0xFFFFFFFF;
 
 uint16_t *palette;
@@ -40,51 +32,46 @@ static void VL_N64_SetVideoMode(int mode)
 {
     if (mode == 0xD)
     {
-        display_init(RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, ANTIALIAS_RESAMPLE_FETCH_ALWAYS);
-        rdp_init();
+        const resolution_t RESOLUTION_320x200 = {320, 200, false};
+        display_init(RESOLUTION_320x200, DEPTH_16_BPP, 2, GAMMA_NONE, ANTIALIAS_RESAMPLE_FETCH_ALWAYS);
+        rdpq_init();
+        rdpq_debug_start();
+
         rdpq_set_fill_color(RGBA32(0,0,0,255));
-
-        display_width = 320;
-        display_height = 240;
-
-        palette = (uint16_t *)memalign(64, sizeof(uint16_t) * 16);
+        palette = (uint16_t *)malloc_uncached_aligned(64, sizeof(uint16_t) * 16);
         assert(palette != NULL);
     }
     else
     {
-        rdp_close();
-        free(palette);
+        rdpq_close();
+        free_uncached(palette);
     }
 }
 
 static void *VL_N64_CreateSurface(int w, int h, VL_SurfaceUsage usage)
 {
-    VL_N64_Surface *surf = (VL_N64_Surface *)malloc(sizeof(VL_N64_Surface));
-    assert(surf != NULL);
-    surf->width = w;
-    surf->height = h;
-    surf->pixels = (uint8_t*)memalign(64, w * h);
-    assert(surf->pixels != NULL);
-    return surf;
+    surface_t surf = surface_alloc(FMT_CI8, w, h);
+    surface_t *psurf = malloc(sizeof(surface_t));
+    memcpy(psurf, &surf, sizeof(surface_t));
+    return psurf;
 }
 
 static void VL_N64_DestroySurface(void *surface)
 {
-    VL_N64_Surface *surf = (VL_N64_Surface *)surface;
-    if (surf->pixels)
-        free(surf->pixels);
-    free(surf);
+    surface_t *psurf = surface;
+    surface_free(psurf);
+    free(psurf);
 }
 
 static long VL_N64_GetSurfaceMemUse(void *surface)
 {
-    VL_N64_Surface *surf = (VL_N64_Surface *)surface;
-    return surf->width * surf->height;
+    surface_t *surf = surface;
+    return surf->stride * surf->height;
 }
 
 static void VL_N64_GetSurfaceDimensions(void *surface, int *w, int *h)
 {
-    VL_N64_Surface *surf = (VL_N64_Surface *)surface;
+    surface_t *surf = surface;
     if (w)
         *w = surf->width;
     if (h)
@@ -108,24 +95,23 @@ static void VL_N64_RefreshPaletteAndBorderColor(void *screen)
         uint16_t c = ((r >> 3) << 11) | ((g >> 3) << 6) | ((b >> 3) << 1) | 0x01; //rgba 5551
         palette[i] = c;
     }
-    data_cache_hit_writeback_invalidate(palette, 16 * 2);
     palette_dirty = true;
 }
 
 static int VL_N64_SurfacePGet(void *surface, int x, int y)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)surface;
-    return ((uint8_t *)surf->pixels)[y * surf->width + x];
+    surface_t *surf = surface;
+    return ((uint8_t *)surf->buffer)[y * surf->width + x];
 }
 
 static void VL_N64_SurfaceRect(void *dst_surface, int x, int y, int w, int h, int colour)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)dst_surface;
+    surface_t *surf = dst_surface;
     for (int _y = y; _y < y + h; ++_y)
     {
-        memset(((uint8_t *)surf->pixels) + (_y * surf->width) + x, colour, CK_Cross_min(w, surf->width - x));
+        memset(((uint8_t *)surf->buffer) + (_y * surf->width) + x, colour, CK_Cross_min(w, surf->width - x));
     }
 }
 
@@ -135,12 +121,12 @@ static void VL_N64_SurfaceRect_PM(void *dst_surface, int x, int y, int w, int h,
     mapmask &= 0xF;
     colour &= mapmask;
 
-    VL_N64_Surface *surf = (VL_N64_Surface *)dst_surface;
+    surface_t *surf = dst_surface;
     for (int _y = y; _y < y + h; ++_y)
     {
         for (int _x = x; _x < x + w; ++_x)
         {
-            uint8_t *p = ((uint8_t *)surf->pixels) + _y * surf->width + _x;
+            uint8_t *p = ((uint8_t *)surf->buffer) + _y * surf->width + _x;
             *p &= ~mapmask;
             *p |= colour;
         }
@@ -150,18 +136,18 @@ static void VL_N64_SurfaceRect_PM(void *dst_surface, int x, int y, int w, int h,
 static void VL_N64_SurfaceToSurface(void *src_surface, void *dst_surface, int x, int y, int sx, int sy, int sw, int sh)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)src_surface;
-    VL_N64_Surface *dest = (VL_N64_Surface *)dst_surface;
+    surface_t *surf = src_surface;
+    surface_t *dest = dst_surface;
     for (int _y = sy; _y < sy + sh; ++_y)
     {
-        memcpy(((uint8_t *)dest->pixels) + (_y - sy + y) * dest->width + x, ((uint8_t *)surf->pixels) + _y * surf->width + sx, sw);
+        memcpy(((uint8_t *)dest->buffer) + (_y - sy + y) * dest->width + x, ((uint8_t *)surf->buffer) + _y * surf->width + sx, sw);
     }
 }
 
 static void VL_N64_SurfaceToSelf(void *surface, int x, int y, int sx, int sy, int sw, int sh)
 {
     _do_audio_update();
-    VL_N64_Surface *srf = (VL_N64_Surface *)surface;
+    surface_t *srf = surface;
     bool directionX = sx > x;
     bool directionY = sy > y;
 
@@ -169,14 +155,14 @@ static void VL_N64_SurfaceToSelf(void *surface, int x, int y, int sx, int sy, in
     {
         for (int yi = 0; yi < sh; ++yi)
         {
-            memmove(((uint8_t *)srf->pixels) + ((yi + y) * srf->width + x), ((uint8_t *)srf->pixels) + ((sy + yi) * srf->width + sx), sw);
+            memmove(((uint8_t *)srf->buffer) + ((yi + y) * srf->width + x), ((uint8_t *)srf->buffer) + ((sy + yi) * srf->width + sx), sw);
         }
     }
     else
     {
         for (int yi = sh - 1; yi >= 0; --yi)
         {
-            memmove(((uint8_t *)srf->pixels) + ((yi + y) * srf->width + x), ((uint8_t *)srf->pixels) + ((sy + yi) * srf->width + sx), sw);
+            memmove(((uint8_t *)srf->buffer) + ((yi + y) * srf->width + x), ((uint8_t *)srf->buffer) + ((sy + yi) * srf->width + sx), sw);
         }
     }
 }
@@ -184,64 +170,64 @@ static void VL_N64_SurfaceToSelf(void *surface, int x, int y, int sx, int sy, in
 static void VL_N64_UnmaskedToSurface(void *src, void *dst_surface, int x, int y, int w, int h)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)dst_surface;
-    VL_UnmaskedToPAL8(src, surf->pixels, x, y, surf->width, w, h);
+    surface_t *surf = dst_surface;
+    VL_UnmaskedToPAL8(src, surf->buffer, x, y, surf->width, w, h);
 }
 
 static void VL_N64_UnmaskedToSurface_PM(void *src, void *dst_surface, int x, int y, int w, int h, int mapmask)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)dst_surface;
-    VL_UnmaskedToPAL8_PM(src, surf->pixels, x, y, surf->width, w, h, mapmask);
+    surface_t *surf = dst_surface;
+    VL_UnmaskedToPAL8_PM(src, surf->buffer, x, y, surf->width, w, h, mapmask);
 }
 
 static void VL_N64_MaskedToSurface(void *src, void *dst_surface, int x, int y, int w, int h)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)dst_surface;
-    VL_MaskedToPAL8(src, surf->pixels, x, y, surf->width, w, h);
+    surface_t *surf = dst_surface;
+    VL_MaskedToPAL8(src, surf->buffer, x, y, surf->width, w, h);
 }
 
 static void VL_N64_MaskedBlitToSurface(void *src, void *dst_surface, int x, int y, int w, int h)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)dst_surface;
-    VL_MaskedBlitClipToPAL8(src, surf->pixels, x, y, surf->width, w, h, surf->width, surf->height);
+    surface_t *surf = dst_surface;
+    VL_MaskedBlitClipToPAL8(src, surf->buffer, x, y, surf->width, w, h, surf->width, surf->height);
 }
 
 static void VL_N64_BitToSurface(void *src, void *dst_surface, int x, int y, int w, int h, int colour)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)dst_surface;
-    VL_1bppToPAL8(src, surf->pixels, x, y, surf->width, w, h, colour);
+    surface_t *surf = dst_surface;
+    VL_1bppToPAL8(src, surf->buffer, x, y, surf->width, w, h, colour);
 }
 
 static void VL_N64_BitToSurface_PM(void *src, void *dst_surface, int x, int y, int w, int h, int colour, int mapmask)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)dst_surface;
-    VL_1bppToPAL8_PM(src, surf->pixels, x, y, surf->width, w, h, colour, mapmask);
+    surface_t *surf = dst_surface;
+    VL_1bppToPAL8_PM(src, surf->buffer, x, y, surf->width, w, h, colour, mapmask);
 }
 
 static void VL_N64_BitXorWithSurface(void *src, void *dst_surface, int x, int y, int w, int h, int colour)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)dst_surface;
-    VL_1bppXorWithPAL8(src, surf->pixels, x, y, surf->width, w, h, colour);
+    surface_t *surf = dst_surface;
+    VL_1bppXorWithPAL8(src, surf->buffer, x, y, surf->width, w, h, colour);
 }
 
 static void VL_N64_BitBlitToSurface(void *src, void *dst_surface, int x, int y, int w, int h, int colour)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)dst_surface;
-    VL_1bppBlitToPAL8(src, surf->pixels, x, y, surf->width, w, h, colour);
+    surface_t *surf = dst_surface;
+    VL_1bppBlitToPAL8(src, surf->buffer, x, y, surf->width, w, h, colour);
 }
 
 static void VL_N64_BitInvBlitToSurface(void *src, void *dst_surface, int x, int y, int w, int h, int colour)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)dst_surface;
-    VL_1bppInvBlitClipToPAL8(src, surf->pixels, x, y, surf->width, w, h, surf->width, surf->height, colour);
+    surface_t *surf = dst_surface;
+    VL_1bppInvBlitClipToPAL8(src, surf->buffer, x, y, surf->width, w, h, surf->width, surf->height, colour);
 }
 
 static int VL_N64_GetActiveBufferId(void *surface)
@@ -260,7 +246,7 @@ static int VL_N64_GetNumBuffers(void *surface)
 static void VL_N64_ScrollSurface(void *surface, int x, int y)
 {
     _do_audio_update();
-    VL_N64_Surface *surf = (VL_N64_Surface *)surface;
+    surface_t *surf = surface;
     int dx = 0, dy = 0, sx = 0, sy = 0;
     int w = surf->width - CK_Cross_max(x, -x), h = surf->height - CK_Cross_max(y, -y);
     if (x > 0)
@@ -290,71 +276,47 @@ static void VL_N64_Present(void *surface, int scrlX, int scrlY, bool singleBuffe
 {
     _do_audio_update();
 
-    VL_N64_Surface *src = (VL_N64_Surface *)surface;
-    data_cache_hit_writeback_invalidate(src->pixels, src->width * src->height);
+    surface_t *omni_fb = surface;
 
-    // We draw the screen from top to bottom.
-    // We can only draw 2048bytes per loop and for simplicity we want it to be a multiple
-    // of the width.
-    int x_per_loop = src->width;
-    int y_per_loop = (2048 / src->width) >= 5 ? 5 : 1;
-    int current_y = 0;
-    int chunk_size = x_per_loop * y_per_loop;
-    assert(chunk_size <= 2048);
-
-    #define TEX_TILE 0
-    #define PALETTE_TILE 2
-    //Eight-bit CI textures do not use the palette number of the tile, since they address the whole 256 TLUT directly.
-    //Just make this zero
-    #define PALETTE_SLOT 0
-
-    disp = display_lock();
-    if (!disp)
+    if (omni_fb->width > 1024)
     {
         return;
     }
 
-    rdp_attach(disp);
-    rdpq_set_scissor(0, 0, display_width, display_height);
-    rdpq_set_other_modes_raw(SOM_CYCLE_COPY | SOM_ENABLE_TLUT_RGB16);
+    surface_t *disp = display_get();
+    rdpq_attach(disp, NULL);
+    rdpq_set_mode_standard();
+
+    // We draw the screen from top to bottom.
+    // We can only draw 2048bytes per loop and for simplicity we want it to be a multiple
+    // of the displays width.
+    int x_per_loop = disp->width;
+    int y_per_loop = 5;
+    int current_y = 0;
+    assert((x_per_loop * y_per_loop) <= 2048);
 
     // Load palette into PALETTE_TILE
     if (palette_dirty)
     {
-        rdpq_set_tile(PALETTE_TILE, FMT_CI4, 0x800 + (PALETTE_SLOT * 0x80), 16, 0);
-        rdpq_set_texture_image(palette, FMT_RGBA16, 16);
-        rdpq_load_tlut(PALETTE_TILE, 0, 15);
+        rdpq_tex_upload_tlut(palette, 0, 16);
         palette_dirty = false;
     }
 
-    uint8_t *ptr = src->pixels + scrlY * x_per_loop;
-    while (current_y < 240)
+    uint8_t *old_buffer = omni_fb->buffer;
+    intptr_t offset_buffer = (intptr_t)omni_fb->buffer + (scrlY * omni_fb->width);
+    omni_fb->buffer = (void *)offset_buffer;
+
+    while (current_y < disp->height)
     {
-        // Load the 8bit indexed texture into TEX_TILE with the associated palette
-        rdpq_set_tile(TEX_TILE, FMT_CI8, 0x0000, x_per_loop, PALETTE_SLOT);
-        rdpq_set_texture_image(ptr, FMT_CI8, x_per_loop);
-        rdpq_load_tile(TEX_TILE, 0, 0, x_per_loop, y_per_loop);
-
-        // Draw a line from RGB_TEX_TILE
-        rdpq_texture_rectangle(TEX_TILE, 0, current_y, x_per_loop, (current_y + 1), scrlX, 0, 1, 1);
-
-        // Standard framebuffer is 200px high, we scale to 240 by adding another line every 5 rows
-        if (y_per_loop == 5)
-        {
-            current_y++;
-            rdpq_texture_rectangle(TEX_TILE, 0, current_y, x_per_loop, (current_y + y_per_loop), scrlX, 0, 1, 1);
-        }
-        else if (current_y % 5 == 0)
-        {
-            current_y++;
-            rdpq_texture_rectangle(TEX_TILE, 0, current_y, x_per_loop, (current_y + 1), scrlX, 0, 1, 1);
-        }
-
+        rdpq_mode_tlut(TLUT_RGBA16);
+        rdpq_tex_upload_sub(TILE0, omni_fb, NULL, scrlX, current_y, x_per_loop + scrlX, current_y + y_per_loop);
+        rdpq_texture_rectangle(TILE0, 0, current_y, x_per_loop, current_y + y_per_loop, scrlX, current_y);
         current_y += y_per_loop;
-        ptr += chunk_size;
     }
+    omni_fb->buffer = old_buffer;
 
-    rdp_auto_show_display(disp);
+    rdpq_detach_show();
+    _do_audio_update();
 }
 
 static void VL_N64_FlushParams()
@@ -369,6 +331,16 @@ static void VL_N64_WaitVBLs(int vbls)
     {
         _do_audio_update();
     } while (timer_ticks() < micros);
+}
+
+static void VL_N64_SyncBuffers(void *surface)
+{
+
+}
+
+static void VL_N64_UpdateRect(void *surface, int x, int y, int w, int h)
+{
+
 }
 
 VL_Backend vl_n64_backend =
@@ -397,6 +369,8 @@ VL_Backend vl_n64_backend =
     .present = &VL_N64_Present,
     .getActiveBufferId = &VL_N64_GetActiveBufferId,
     .getNumBuffers = &VL_N64_GetNumBuffers,
+    .syncBuffers = VL_N64_SyncBuffers,
+    .updateRect = VL_N64_UpdateRect,
     .flushParams = &VL_N64_FlushParams,
     .waitVBLs = &VL_N64_WaitVBLs
 };
